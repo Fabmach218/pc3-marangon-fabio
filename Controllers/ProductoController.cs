40af828543496089b3f1c3b76f5bea03bbb550bd
Fabio Marangon <fabio_marangon@usmp.pe>
2021-10-19 18:17:58 -0500

    Programando controlador

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using pc3_marangon_fabio.Models;
 using pc3_marangon_fabio.Data;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace pc3_marangon_fabio.Controllers
 {
@@ -22,12 +25,44 @@ namespace pc3_marangon_fabio.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            
+            var fechaActual = DateTime.Now;
+            var dia = fechaActual.DayOfYear - 5;
+            
+            var productos = _context.DataProductos.Include(c => c.Categoria).Where(p => p.Fecha.DayOfYear >= dia);
+            return View(productos);
         }
 
         public IActionResult Crear()
         {
+
+            var categorias = _context.DataCategorias.ToList();
+
+            List<SelectListItem> items = categorias.ConvertAll(c => {
+
+                return new SelectListItem(){
+
+                    Text = c.Nombre,
+                    Value = c.Id.ToString(),
+                    Selected = false
+                };
+            });
+          
+            ViewBag.items = items;
+
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Producto producto, int categoriaId){
+
+            Categoria categoria = _context.DataCategorias.Find(categoriaId);
+            producto.Categoria = categoria;
+            _context.Add(producto);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
         }
 
         
